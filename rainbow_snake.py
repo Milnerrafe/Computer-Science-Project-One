b530889dc1debb13fc182fb7d31414249c53d61e
Rafe Milner <milnerrafe@outlook.com>
2026-03-28 14:50:37 +0800

    Cleaned up rainbow snake

--- rainbow_snake.py
+++ rainbow_snake.py
@@ -1,5 +1,4 @@
 class color:
-    ostype = 0
 
     error = "*/error/*"
     errorOFF = "*/error:OFF/*"
@@ -22,237 +21,232 @@ class color:
     hexOFF = ""
 
     @classmethod
-    def output(cls, string):
+    def whichos(cls):
         import os
-        import re
         import sys
 
         if "idlelib" in sys.modules:
             try:
                 shell_connect = sys.stdout.shell
-                cls.ostype = 1
+                return "idle"
             except AttributeError:
-                cls.ostype = -1
+                return "unknown"
         else:
-            cls.ostype = 2
+            return "terminal"
 
-        if cls.ostype == 1:
-            string = string.replace("\n", "{color.newline()}")
+        
 
-            styleTable = {
+    @classmethod
+    def output(cls, string):
+
+        match cls.whichos():
+
+            case "idle":
+
+                import sys
+
+                shell_connect = sys.stdout.shell
+
+                string = string.replace("\n", "{color.newline()}")
+                
+                styleTable = {
                 "error": ("shell_connect.write(f'''", "''', 'COMMENT')"),
                 "warning": ("shell_connect.write(f'''", "''', 'KEYWORD')"),
                 "success": ("shell_connect.write(f'''", "''', 'STRING')"),
                 "information": ("shell_connect.write(f'''", "''', 'stdout')"),
                 "important": ("shell_connect.write(f'''", "''', 'BUILTIN')"),
                 "bold": ("shell_connect.write(f'''", "''', 'SYNC')"),
-            }
+                }
 
-            pattern = r"\*/(error|warning|success|information|important|context|bold)(?::(OFF))?/\*"
+                pattern = r"\*/(error|warning|success|information|important|context|bold)(?::(OFF))?/\*"
 
-            import re
+                import re
 
-            def replOStypeone(m):
-                esccode = m.group(1)
-                is_off = m.group(2) == "OFF"
+                def replOStypeone(m):
+                    esccode = m.group(1)
+                    is_off = m.group(2) == "OFF"
 
-                if esccode not in styleTable:
-                    return ""
+                    if esccode not in styleTable:
+                        return ""
 
-                open_code, close_code = styleTable[esccode]
-                return close_code if is_off else open_code
+                    open_code, close_code = styleTable[esccode]
+                    return close_code if is_off else open_code
 
-            result = re.sub(pattern, replOStypeone, string)
 
-            patterntwo = r"(shell_connect\.write\((?:[^()]*|\([^()]*\))*\))"
+                result = re.sub(pattern, replOStypeone, string)
 
-            parts = re.split(patterntwo, result)
+                patterntwo = r"(shell_connect\.write\((?:[^()]*|\([^()]*\))*\))"
 
-            output = []
+                parts = re.split(patterntwo, result)
 
-            for part in parts:
-                if re.match(patterntwo, part):
-                    output.append(part)
-                else:
-                    output.append(f"shell_connect.write(f'''{part}''', 'stdout')")
+                output = []
 
-            result = "\n".join(output)
+                for part in parts:
+                    if re.match(patterntwo, part):
+                        output.append(part)
+                    else:
+                        output.append(f"shell_connect.write(f'''{part}''', 'stdout')")
 
-            exec(result)
+                codetorun = "\n".join(output)
 
-            shell_connect.write("\n", "stdout")
+                exec(codetorun)
+                
 
-        elif cls.ostype == 2:
-            styleTable = {
+                shell_connect.write("\n", "stdout")
+
+
+
+            case "terminal":
+
+                styleTable = {
                 "error": "\x1b[1m\x1b[38;2;255;255;255m\x1b[48;2;255;60;60m",
                 "warning": "\x1b[1m\x1b[38;2;0;0;0m\x1b[48;2;255;219;60m",
                 "success": "\x1b[1m\x1b[38;2;255;255;255m\x1b[48;2;57;226;0m",
                 "information": "\x1b[1m\x1b[38;2;255;255;255m\x1b[48;2;60;135;255m",
                 "important": "\x1b[1m\x1b[38;2;255;255;255m\x1b[48;2;255;60;209m",
                 "bold": "\x1b[1m",
-            }
+                }
 
-            pattern = r"\*/(error|warning|success|information|important|context|bold)(?::(OFF))?/\*"
+                pattern = r"\*/(error|warning|success|information|important|context|bold)(?::(OFF))?/\*"
 
-            import re
+                import re
 
-            def replOStypetwo(m):
-                esccode = m.group(1)
-                is_off = m.group(2) == "OFF"
+                def replOStypetwo(m):
+                    esccode = m.group(1)
+                    is_off = m.group(2) == "OFF"
 
-                if esccode not in styleTable:
-                    return ""
+                    if esccode not in styleTable:
+                        return ""
 
-                open_esccode = styleTable[esccode]
-                return "\x1b[0m" if is_off else open_esccode
+                    open_esccode = styleTable[esccode]
 
-            result = re.sub(pattern, replOStypetwo, string)
+                    return "\x1b[0m" if is_off else open_esccode
 
-            print(result)
+                result = re.sub(pattern, replOStypetwo, string)
 
-        elif cls.ostype == -1:
-            raise Exception("Rainbow-Snake does not support your os or interpreter")
-        else:
-            raise Exception("Rainbow-Snake does not support your os or interpreter")
+                print(result)
+                
+
+            case "unknown":
+
+                import re
+
+                removedcodes = re.sub(r"\*/(error|warning|success|information|important|context|bold)(?::(OFF))?/\*", '', string)
+                print(removedcodes)
+
+            case _:
+
+                import re
+
+                removedcodes = re.sub(r"\*/(error|warning|success|information|important|context|bold)(?::(OFF))?/\*", '', string)
+                print(removedcodes)
+
+
 
     @classmethod
     def hextext(cls, hex):
-        import os
-        import sys
 
-        if "idlelib" in sys.modules:
-            try:
-                shell_connect = sys.stdout.shell
-                cls.ostype = 1
-            except AttributeError:
-                cls.ostype = -1
-        else:
-            cls.ostype = 2
+        match cls.whichos():
 
-        if cls.ostype == 1:
-            return ""
-        elif cls.ostype == 2:
-            cls.hexOFF = "\x1b[0m"
+            case "terminal":
+                cls.hexOFF = "\x1b[0m"
 
-            hexdehashtag = hex.replace("#", "")
+                hexdehashtag = hex.replace("#", "")
 
-            if len(hexdehashtag) == 6:
-                try:
-                    red = int(hexdehashtag[0] + hexdehashtag[1], 16)
-                except ValueError:
-                    raise Exception(
+                if len(hexdehashtag) == 6:
+                    try:
+                        red = int(hexdehashtag[0] + hexdehashtag[1], 16)
+
+                    except ValueError:
+                        raise Exception(
                         f"{hex} Is not a correct hex code. Format your hex code to look like, #FFFFFF or FFFFFF"
-                    )
+                        )
 
-                try:
-                    green = int(hexdehashtag[2] + hexdehashtag[3], 16)
-                except ValueError:
-                    raise Exception(
+                    try:
+                        green = int(hexdehashtag[2] + hexdehashtag[3], 16)
+
+                    except ValueError:
+                        raise Exception(
                         f"{hex} Is not a correct hex code. Format your hex code to look like, #FFFFFF or FFFFFF"
-                    )
+                        )
 
-                try:
-                    blue = int(hexdehashtag[4] + hexdehashtag[5], 16)
-                except ValueError:
-                    raise Exception(
+                    try:
+                        blue = int(hexdehashtag[4] + hexdehashtag[5], 16)
+
+                    except ValueError:
+                        raise Exception(
                         f"{hex} Is not a correct hex code. Format your hex code to look like, #FFFFFF or FFFFFF"
-                    )
+                        )
 
-            else:
-                raise Exception(
-                    f"{hex} Is not a correct hex code. Format your hex code to look like, #FFFFFF or FFFFFF"
-                )
+                    return f"\x1b[38;2;{red};{green};{blue}m"
+                
 
-            return f"\x1b[38;2;{red};{green};{blue}m"
-        elif cls.ostype == -1:
-            raise Exception("Rainbow-Snake does not support your os or interpreter")
-        else:
-            raise Exception("Rainbow-Snake does not support your os or interpreter")
+            case _:
+                return ""
 
     @classmethod
     def hexbg(cls, hex):
-        import os
-        import sys
 
-        if "idlelib" in sys.modules:
-            try:
-                shell_connect = sys.stdout.shell
-                cls.ostype = 1
-            except AttributeError:
-                cls.ostype = -1
-        else:
-            cls.ostype = 2
+        match cls.whichos():
 
-        if cls.ostype == 1:
-            return ""
-        elif cls.ostype == 2:
-            cls.hexOFF = "\x1b[0m"
+            case "terminal":
+                cls.hexOFF = "\x1b[0m"
 
-            hexdehashtag = hex.replace("#", "")
+                hexdehashtag = hex.replace("#", "")
 
-            if len(hexdehashtag) == 6:
-                try:
-                    red = int(hexdehashtag[0] + hexdehashtag[1], 16)
-                except ValueError:
-                    raise Exception(
+                if len(hexdehashtag) == 6:
+                    try:
+                        red = int(hexdehashtag[0] + hexdehashtag[1], 16)
+
+                    except ValueError:
+                        raise Exception(
                         f"{hex} Is not a correct hex code. Format your hex code to look like, #FFFFFF or FFFFFF"
-                    )
+                        )
 
-                try:
-                    green = int(hexdehashtag[2] + hexdehashtag[3], 16)
-                except ValueError:
-                    raise Exception(
+                    try:
+                        green = int(hexdehashtag[2] + hexdehashtag[3], 16)
+
+                    except ValueError:
+                        raise Exception(
                         f"{hex} Is not a correct hex code. Format your hex code to look like, #FFFFFF or FFFFFF"
-                    )
+                        )
 
-                try:
-                    blue = int(hexdehashtag[4] + hexdehashtag[5], 16)
-                except ValueError:
-                    raise Exception(
+                    try:
+                        blue = int(hexdehashtag[4] + hexdehashtag[5], 16)
+
+                    except ValueError:
+                        raise Exception(
                         f"{hex} Is not a correct hex code. Format your hex code to look like, #FFFFFF or FFFFFF"
-                    )
+                        )
 
-            else:
-                raise Exception(
-                    f"{hex} Is not a correct hex code. Format your hex code to look like, #FFFFFF or FFFFFF"
-                )
+                    return f"\x1b[48;2;{red};{green};{blue}m"
+                
 
-            return f"\x1b[48;2;{red};{green};{blue}m"
-        elif cls.ostype == -1:
-            raise Exception("Rainbow-Snake does not support your os or interpreter")
-        else:
-            raise Exception("Rainbow-Snake does not support your os or interpreter")
+            case _:
+                return ""
+       
 
     @classmethod
     def clear(cls):
         import os
-        import sys
 
-        if "idlelib" in sys.modules:
-            try:
-                shell_connect = sys.stdout.shell
-                cls.ostype = 1
-            except AttributeError:
-                cls.ostype = -1
-        else:
-            cls.ostype = 2
+        match cls.whichos():
 
-        if cls.ostype == 1:
-            print("\n" * 5)
-            print("\n" * 5)
-            print("\n" * 5)
-            print("\n" * 5)
-            print("\n" * 5)
-            print("\n" * 5)
-        elif cls.ostype == 2:
-            if os.name == "nt":
-                os.system("cls")
-            else:
-                os.system("clear")
-        elif cls.ostype == -1:
-            raise Exception("Rainbow-Snake does not support your os or interpreter")
-        else:
-            raise Exception("Rainbow-Snake does not support your os or interpreter")
+            case "terminal":
+                if os.name == "nt":
+                   os.system("cls")
+                else:
+                   os.system("clear")
+               
+
+            case _:
+                 print("\n" * 5)
+                 print("\n" * 5)
+                 print("\n" * 5)
+                 print("\n" * 5)
+                 print("\n" * 5)
+                 print("\n" * 5)
+
 
     @classmethod
     def newline(cls):
