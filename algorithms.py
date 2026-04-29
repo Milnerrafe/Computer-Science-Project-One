138fe6e786efb7bdd198a8553f04258414f51601
Rafe Milner <milnerrafe@outlook.com>
2026-04-29 21:30:28 +0800

    Added Code

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,4 @@
-employeeData = [
+employeesData = [
     ["Bob", "Manager", [1, 3, 9, 10, 11, 2, 1]],
     ["Rob", "Barista", [6, 7, 5, 8, 9, 4, 6]],
     ["Dom", "Cleaner", [7, 8, 3, 2, 4, 7, 0]],
@@ -6,17 +6,21 @@ employeeData = [
 
 name = 0
 position = 1
-hoursWorked = 2
+days = 2
 
+iterationNumber = 0
+for employee in employeesData:
+    iterationNumber += 1
+    hoursWorked = 0
 
-o = 1
-for i in employeeData:
-    h = 0
+    for hours in employee[days]:
+        hoursWorked += hours
 
-    for y in i[hoursWorked]:
-        h += y
+    if employee[position] == "Manager":
+        pay = hoursWorked * 30
+    else:
+        pay = hoursWorked * 26
 
     print(
-        f"Employee Number {o}, name is {i[name]} and their position is {i[position]}, this week they worked a total of {h} hours"
+        f"Employee Number {iterationNumber}, name is {employee[name]} and their position is {employee[position]}, this week they worked a total of {hoursWorked} hours and should be payed ${pay} this week."
     )
-    o += 1
