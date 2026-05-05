3df3fc54bb9740da8f247f01c15e3812834696e4
Rafe Milner <milnerrafe@outlook.com>
2026-05-05 22:55:57 +0800

    Completed Input

--- better.py
+++ better.py
@@ -2,7 +2,13 @@
 from datetime import date
 
 # Declaration of the Array of Employees
-employeeData = []
+employeeData = [
+    {
+        "name": "Meg Docherty",
+        "position": "Manager",
+        "hoursperday": [1, 3, 9, 10, 11, 2, 1],
+    }
+]
 
 
 # Clear function
@@ -36,6 +42,10 @@ def clear():
             os.system("clear")
 
 
+def viewEmployeedata():
+    print("hi")
+
+
 # Function to check whether an employee exists in the data structure.
 def checkEmployee(name):
     # IntelliSense information
@@ -50,13 +60,14 @@ def checkEmployee(name):
     # Using iteration control structure, check whether the name provided is in the list of employees.
     for index, employee in enumerate(employeeData):
         # If the name is in the list of employees, return false and provide the index to that data.
-        if name == employee.name:
-            return False, index
+        # Strings modified to remove spaces and lowercase all uppercase characters.
+        if "".join(name.split()).lower() == "".join(employee["name"].split()).lower():
+            return True, index
         else:
             continue
 
     # If the name is not in the data structure, return true and do not provide an index.
-    return True, None
+    return False, None
 
 
 # Enter employee information and hours function.
@@ -71,6 +82,9 @@ def enterEmployeehours():
 
     # Define the step that the loop is in
     step = 0
+    name = ""
+    position = ""
+    index = None
 
     # Define function loop
     while True:
@@ -78,24 +92,164 @@ def enterEmployeehours():
         clear()
 
         print("Enter Employee hours: \n")
+        print("Press [B] to go back to home. \n")
 
-        # If there is an error message, print it, Otherwise, print a blank new line.
-        print(errormessage) if errormessage else print("\n")
+        # If the user has entered an acceptable name, move the step from 0 to 1.
+        if name:
+            step = 1
+
+        # If the user has entered an position, move the step from 1 to 2.
+        if index:
+            step = 2
 
         if step == 0:
+            # If there is an error message, print it.
+            if errormessage:
+                print(errormessage + "\n")
+                errormessage = ""
+
             # Try accept expression for name input to allow for clean exiting, back to the main screen.
             try:
-                name = input(
+                nameInput = input(
                     "What is the name of the employee you would like to add:   "
                 )
             except KeyboardInterrupt:
                 clear()
                 break
 
-            if checkEmployee(name):
-                print("yes")
+            # Check if the user would like to return to the homepage.
+
+            if nameInput.lower() == "b":
+                clear()
+                break
+
+            # Check if the employee already exists.
+            employeeExists, indexOfemployee = checkEmployee(nameInput)
+
+            if employeeExists:
+                errormessage = "That name is already used, Enter a different name."
             else:
-                print("no")
+                name = nameInput
+                continue
+        else:
+            # Show user visual confirmation that they have entered a name.
+            print(f"What is the name of the employee you would like to add: {name}")
+
+        if step == 1:
+            # If there is an error message, print it.
+            if errormessage:
+                print(errormessage + "\n")
+                errormessage = ""
+
+            # Ask the user which position the employee will fill.
+            position = input(
+                "Which position does this employee have? [1]-Manager, [2]-Barista, [3]-Cleaner:  "
+            )
+
+            # Match the user's input position. If there isn't a position, return an error.
+            match position:
+                case "1":
+                    position = "Manager"
+                case "2":
+                    position = "Barista"
+                case "3":
+                    position = "Cleaner"
+                case _:
+                    position = ""
+                    errormessage = "Please select one of the options; [1]-Manager, [2]-Barista, [3]-Cleaner"
+                    continue
+
+            # Create dictionary based on the given information then calculate its index in the employee data array.
+            employeeObject = dict(
+                name=name,
+                position=position,
+                hoursperday=[0, 0, 0, 0, 0, 0, 0],
+                holidays=[False, False, False, False, False, False, False],
+                payperday=[0, 0, 0, 0, 0, 0, 0],
+                totalpay=0,
+            )
+            employeeData.append(employeeObject)
+            index = len(employeeData) - 1
+
+        else:
+            # Show user visual confirmation that they have entered a Position.
+            print(f"Which position does this employee have?: {position}")
+
+        if step == 2:
+            # Print message to user to inform them to enter the hours for the employee.
+            # Getting the employees name from the index and dictionary created earlier,
+            # ensuring that if there is an error then it is thrown immediately.
+            print(f"\n \nEnter Hours for {employeeData[index]['name']}")
+
+            days = [
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday",
+            ]
+
+            for dayNumber, day in enumerate(days):
+                internalErrormessage = ""
+                while True:
+                    # Have the first day Feature a message referencing the Employees's name.
+                    if day == "Monday":
+                        inputString = f"Enter the hours worked by {employeeData[index]['name']} for {day}:  "
+                    else:
+                        inputString = f"Enter the hours worked for {day}:  "
+
+                    # Print internal error message.
+                    if internalErrormessage:
+                        print(internalErrormessage)
+
+                    # Error handling for float numbers.
+                    try:
+                        hoursworked = float(input(inputString))
+                    except ValueError:
+                        internalErrormessage = (
+                            "Please enter a number between 0.1 and 24"
+                        )
+                        continue
+
+                    # Check whether the input is in range.
+                    if hoursworked < 0.1 or hoursworked > 24:
+                        internalErrormessage = (
+                            "Please enter a number between 0.1 and 24"
+                        )
+                        continue
+                    else:
+                        employeeData[index]["hoursperday"][dayNumber] = hoursworked
+
+                    internalinternalErrormessage = ""
+                    while True:
+                        # If there is an error with the user entering yes or no, then it is displayed.
+                        if internalinternalErrormessage:
+                            print(internalinternalErrormessage)
+
+                        # Input for holidays
+                        holdayInput = input(f"Is {day}, a holday? [Y]es/[N]o:  ")
+
+                        if holdayInput.lower() == "yes" or holdayInput.lower() == "y":
+                            employeeData[index]["holidays"][dayNumber] = True
+                            break
+                        elif holdayInput.lower() == "no" or holdayInput.lower() == "n":
+                            break
+                        else:
+                            internalinternalErrormessage = "Please enter yes or no"
+                            continue
+
+                    # Move to step 3 using the selection control structure of the iteration loop.
+                    step = 3
+                    break
+
+            if step == 3:
+                input(employeeData)
+
+            break
+
+    viewEmployeedata()
 
 
 # Main function (The main function controls the selection of which function
